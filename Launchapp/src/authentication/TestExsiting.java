85e6fedda0e9c36c5597326f8ac77db42acbe807
lathavya.buddya.bc <lathavya.buddya.bc@BDC3-DX-430TY83.dir.svc.accenture.com>
2018-10-10 16:23:57 +0530

    test

--- Launchapp/src/authentication/TestExsiting.java
+++ Launchapp/src/authentication/TestExsiting.java
@@ -1,14 +1,22 @@
 package authentication;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+
+
 public class TestExsiting {
-public static void main(String[] args) throws InterruptedException {
+public static void main(String[] args) throws InterruptedException, IOException {
 	System.setProperty("webdriver.chrome.driver", "./Software/chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
@@ -19,9 +27,14 @@ Pageobjectmodel p = new Pageobjectmodel(driver);
 p.ClickOnHomeButton();
 p.ClickonPlans();
 p.ClickonBiologyPlan();
-	 driver.findElement(By.xpath("(//tr[@class='protocol-row IE8-zebra-row'])[1]"));
-	 Thread.sleep(3000);
+	 WebElement exist = driver.findElement(By.xpath("(//tr[@class='protocol-row IE8-zebra-row'])[1]"));
 	 Actions ac= new Actions(driver);
+	 ac.moveToElement(exist);
 	 ac.doubleClick().perform();
+	Thread.sleep(30000);
+	TakesScreenshot js = (TakesScreenshot)driver;
+	File scr = js.getScreenshotAs(OutputType.FILE);
+	File dst = new File("C:\\Users\\lathavya.buddya.bc\\Desktop\\New folder\\SS.jpeg");
+	FileUtils.copyFile(scr, dst);
 }
 }
